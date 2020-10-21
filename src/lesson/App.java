3a6d54ea44170cb91e387b9d881244c7541987b7
dpals-103 <dpals103@gmail.com>
2020-10-21 11:12:47 +0900

    게시물삭제_수업본

--- src/lesson/App.java
+++ src/lesson/App.java
@@ -4,20 +4,31 @@ import java.util.Scanner;
 
 public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 
-	// 공공재 article 클래스 생성
+	
+	// 공공재생성
+	
+	Article[] articles = new Article[3]; 
 
 	int articleSize = 0;
-	Article[] articles = new Article[3];
-
-	// 게시물 번호 초기값
 	int lastArticleId = 0;
-
+	
+	
+	int articleSize() {
+		return articleSize;
+	}
+	
+	
 	Article getArticle(int id) {
-
-		return articles[id - 1];
+		int index = getIndexOf(id); 
+		
+		if (index == -1) {  
+			return null;
+		}
+		return articles[index];
 	}
 
-	// 인덱스 찾기
+	
+	// 인덱스 찾기 함수 
 	public int getIndexOf(int id) {
 		for (int i = 0; i < articles.length; i++) {
 			if (id == articles[i].id) {
@@ -28,7 +39,7 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 		return -1;
 	}
 
-	// 인덱스 지우기
+	// 인덱스 지우기 함수 
 	public void remove(int index) {
 		for (int i = index + 1; i < articleSize; i++) {
 			articles[i - 1] = articles[i];
@@ -37,11 +48,10 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 		articleSize--;
 	}
 
+	
+	
 	public void run() { // 실제 작동구간
 
-		for (int i = 0; i < articles.length; i++) {
-			articles[i] = new Article();
-		}
 
 		// 명령어 스캐너 기능 만들기
 		Scanner scanner = new Scanner(System.in);
@@ -54,20 +64,31 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 			System.out.printf("명령어 ) ");
 			String command = scanner.nextLine();
 
+			
+			
 			// 게시물 삭제
 			if (command.startsWith("article delete ")) {
 
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 
 				System.out.printf("%d번 게시글이 삭제되었습니다\n", inputedId);
-
+				
+				Article article = getArticle(inputedId);
+				
+				if(article == null) {
+					System.out.printf("%d번 글이 존재하지 않습니다/n",inputedId);
+				}
+				
 				int index = getIndexOf(inputedId);
 
 				if (index != -1) {
 					remove(index);
 				}
 			}
-
+		
+	
+			
+			
 			// 게시물등록
 			if (command.equals("article add")) {
 				System.out.println("==게시물등록==");
@@ -88,55 +109,69 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 				System.out.printf("내용 : ");
 				body = scanner.nextLine();
 
-				Article article = getArticle(id);
+				Article article = new Article(); 
+				//무작위로 큰 공간을 만드는게 아니고, 추가되는만큼(필요한만큼)만 공간을 만든다 
 
 				article.id = id;
 				article.title = title;
 				article.body = body;
 
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
+				
+				articles[articleSize] = article;
+				//articles[0] = article; 
 
 				articleSize++;
 			}
+			
+			
+			
 
 			// 게시물리스트
 			if (command.equals("article list")) {
 				System.out.println("==게시물리스트==");
 
-				if (lastArticleId == 0) {
+				if ( articleSize() == 0 ) {
 					System.out.println("==게시물이 존재하지 않습니다==");
 					continue;
 				}
 
 				System.out.println("번호/제목");
 
-				for (int i = 1; i <= articleSize; i++) {
-					Article article = getArticle(i);
+				for ( int i = 0;  i < articleSize() ; i++ ) {
+					
+					Article article = articles[i];
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 
 				}
 			}
 
+			
+			
+			
 			// 게시물상세보기
 
 			if (command.startsWith("article detail ")) {
 				System.out.println("==게시물상세==");
 
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
-
-				if (inputedId == 0 || inputedId > lastArticleId) {
+				
+				Article article = getArticle(inputedId);
+				
+				if ( article == null) {
 					System.out.printf("%d번 글은 존재하지 않습니다\n", inputedId);
 					continue;
 				}
-
-				Article article = getArticle(inputedId);
 
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
 
 			}
+			
+			
+			
 
 			// 시스템 종료
 			if (command.equals("article exit")) {
