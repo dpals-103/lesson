f01bf0e6ace418533de4ed2f5cdba6a8aad220b7
dpals-103 <dpals103@gmail.com>
2020-10-29 12:06:38 +0900

    List 적용_이해못함_복습필요

--- src/lesson/App.java
+++ src/lesson/App.java
@@ -1,28 +1,24 @@
 package lesson;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 
-	private Article[] articles;
+	private List<Article> articles;
 	private int lastArticleId;
-	private int articleSize;
 
 	// 생성자 메서드 (자동으로 초기화)
 	public App() {
-		articles = new Article[32];
 		lastArticleId = 0;
-		articleSize = 0;
+		articles = new ArrayList<>();
 
 		for (int i = 0; i < 32; i++) {
 			add("제목" + (i + 1), "내용" + (i + 1));
 		}
-	}
-
-	private int articleSize() {
-		return articleSize;
 	}
 
 	private Article getArticle(int id) {
@@ -31,52 +27,31 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 		if (index == -1) {
 			return null;
 		}
-		return articles[index];
+		return articles.get(index);
 	}
 
-	// 게시물 정보 저장하는 함수
 	private int add(String title, String body) {
 
 		// 게시물 무한대로 저장하기 ( 새 저장소 업체와 계약하기 )
 		// 배열은 증가할 수 없다! -> 증가한게 아니라 공간이 큰 새로운 저장소로 옮겨가는 것
 
-		if (isArticlesFull()) {
-			System.out.printf("==배열 사이즈 증가 (%d => %d) ==\n", articles.length, articles.length * 2);
-
-			Article[] newArticles = new Article[articles.length * 2];
-
-			for (int i = 0; i < articles.length; i++) {
-				newArticles[i] = articles[i];
-			}
-
-			articles = newArticles;
-		}
-
 		Article article = new Article();
-		// 무작위로 큰 공간을 만드는게 아니고, 추가되는만큼(필요한만큼)만 공간을 만든다
 
 		article.id = lastArticleId + 1;
 		article.title = title;
 		article.body = body;
 
-		articles[articleSize] = article;
-		// articles[0] = article;
-
-		articleSize++;
+		articles.add(article);
 		lastArticleId = article.id;
 
 		return article.id;
 
 	}
 
-	private boolean isArticlesFull() {
-		return articleSize == articles.length;
-	}
-
 	// 인덱스 찾기 함수
 	private int getIndexOf(int id) {
-		for (int i = 0; i < articleSize; i++) {
-			if (id == articles[i].id) {
+		for (int i = 0; i < articles.size(); i++) {
+			if (articles.get(i).id == id) {
 				return i;
 			}
 		}
@@ -92,12 +67,7 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 		if (index == -1) {
 			return;
 		}
-
-		for (int i = index + 1; i < articleSize; i++) {
-			articles[i - 1] = articles[i];
-		}
-
-		articleSize--;
+		articles.remove(index);
 	}
 
 	// 게시물 수정하기 함수
@@ -158,18 +128,16 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 
 			// 게시물리스트
 			if (command.startsWith("article list ")) {
-				
+
 				int page = Integer.parseInt(command.split(" ")[2]);
-				
-				if ( page <= 1) {
+
+				if (page <= 1) {
 					page = 1;
 				}
-				
+
 				System.out.println("==게시물리스트==");
 
-				
-
-				if (articleSize() == 0) {
+				if (articles.size() == 0) {
 					System.out.println("==게시물이 존재하지 않습니다==");
 					continue;
 				}
@@ -177,7 +145,7 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 				System.out.println("번호/제목");
 
 				int itemsInAPage = 10; // 1페이지당 게시물 갯수
-				int startPos = articleSize() - 1; // 시작 인덱스 초기값
+				int startPos = articles.size() - 1; // 시작 인덱스 초기값
 				startPos -= (page - 1) * itemsInAPage; // 각 페이지 별 시작점
 				int endPos = startPos - (itemsInAPage - 1); // 각 페이지 별 끝점
 
@@ -193,62 +161,49 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 				// 최신 게시글 순으로 리스팅
 				for (int i = startPos; i >= endPos; i--) {
 
-					Article article = articles[i];
+					Article article = articles.get(i);
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
 
 			}
 
-
 			if (command.startsWith("article search ")) {
-				
-				String[] commandBits = command.split(" "); 
-				
+
+				String[] commandBits = command.split(" ");
+
 				String searchKeyword = commandBits[2];
-				
-				int page = 1; 
-				
+
+				int page = 1;
+
 				if (commandBits.length >= 4) {
 					page = Integer.parseInt(command.split(" ")[3]);
 				}
-				
-				if ( page <= 1) {
+
+				if (page <= 1) {
 					page = 1;
 				}
-				
+
 				System.out.println("==게시물검색==");
-				
-				
-				int searchResultLen = 0; 
-				
-				//검색된 결과의 수를 먼저 구하기 
+
+				List<Article> searchResultArticles = new ArrayList<>(); 
+
+				// 검색된 결과의 수를 먼저 구하기
 				for (Article article : articles) {
-					if ( article.title.contains(searchKeyword)) {
-						searchResultLen++; 
+					if (article.title.contains(searchKeyword)) {
+						searchResultArticles.add(article);
 					}
 				}
 
-				Article[] searchResult = new Article[searchResultLen]; 
-				
-				int searchResultIndex = 0; 
-				
-				for (Article article : articles) {
-					if (article.title.contains(searchKeyword)) {
-						searchResult[searchResultIndex]=article;
-						searchResultIndex++;
-					}
-				}
-				
-				if (searchResult.length == 0) {
+				if (searchResultArticles.size() == 0) {
 					System.out.println("검색결과가 존재하지 않습니다");
-					continue; 
+					continue;
 				}
-				
+
 				System.out.println("번호/제목");
 
 				int itemsInAPage = 10; // 1페이지당 게시물 갯수
-				int startPos = searchResultLen - 1; // 시작 인덱스 초기값
+				int startPos = searchResultArticles.size() - 1; // 시작 인덱스 초기값
 				startPos -= (page - 1) * itemsInAPage; // 각 페이지 별 시작점
 				int endPos = startPos - (itemsInAPage - 1); // 각 페이지 별 끝점
 
@@ -264,7 +219,7 @@ public class App { // 변수선언 (공유공간, 공통메모..), 실행x
 				// 최신 게시글 순으로 리스팅
 				for (int i = startPos; i >= endPos; i--) {
 
-					Article article = articles[i];
+					Article article = searchResultArticles.get(i);
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
